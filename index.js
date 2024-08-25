cdea1fac62f4a60505961f77d6aa3c5dd8d89faa
rfaraf <mohsinarfat53@gamil.com>
2024-08-25 09:15:15 +0600

    add all crud

--- index.js
+++ index.js
@@ -43,14 +43,56 @@ async function run() {
 
     // search books
     app.get("/searchedBooks", async (req, res) => {
-      const search = req.query.search;
-      console.log(search);
-      let query = {
-        title: { $regex: search, $options: "i" },
-      };
-
-      const result = await bookCollection.find(query).toArray();
+      const result = await bookCollection.find().toArray();
       res.send(result);
+    });
+
+    // get all books data for pagination
+    app.get("/all-books", async (req, res) => {
+      const page = parseInt(req.query.page) - 1;
+      const size = parseInt(req.query.size);
+      console.log("page=>", page, "size=>", size);
+
+      const filter = req.query.filter;
+      console.log("filter=>", filter);
+
+      const sort = req.query.sort;
+      console.log("sort=>", sort);
+
+      const search = req.query.search;
+      console.log("search=>", search);
+
+      let query = {
+        bookName: { $regex: search, $options: "i" },
+      };
+      // if (filter) query = { category: filter } একই if (filter) query.category = filter;
+      if (filter) query.category = filter;
+
+      let options = {};
+      if (sort) options = { sort: { price: sort === "asc" ? 1 : -1 } };
+
+      const result = await bookCollection
+        .find(query, options)
+        .skip(page * size)
+        .limit(size)
+        .toArray();
+      res.send(result);
+    });
+
+    // get all books data count
+    app.get("/books-count", async (req, res) => {
+      const filter = req.query.filter;
+      console.log("filter=>", filter);
+
+      const search = req.query.search;
+
+      let query = {
+        bookName: { $regex: search, $options: "i" },
+      };
+      if (filter) query.category = filter;
+
+      const count = await bookCollection.countDocuments(query);
+      res.send({ count });
     });
 
     // Send a ping to confirm a successful connection
